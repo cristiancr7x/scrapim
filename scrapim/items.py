00f924570097acbc5221165a8b035bd0861d6dd7
cristian <cristian_amejia@soy.sena.edu.co>
2025-04-10 17:30:59 +0000

    Add files via upload

--- scrapim/items.py
+++ scrapim/items.py
@@ -1,4 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
 import scrapy
 
-class ScrapimItem(scrapy.Item):
+
+class MyprojectItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
     pass
+
+
+class URLItem(scrapy.Item):
+    # Campo para almacenar la URL recolectada
+    url = scrapy.Field()
+    # Campo para almacenar el dominio de la URL
+    domain = scrapy.Field()
+    # Campo para almacenar la palabra clave que coincidió
+    keyword = scrapy.Field()
