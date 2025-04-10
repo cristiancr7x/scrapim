389fb4ea4bf0435ca09a01a6bc96e18836a7a910
cristian <cristian_amejia@soy.sena.edu.co>
2025-04-10 17:32:25 +0000

    Add files via upload

--- scrapim/spiders/sqli_url_collector.py
+++ scrapim/spiders/sqli_url_collector.py
@@ -1,12 +1,37 @@
-import scrapy
+from scrapy import Spider
+from urllib.parse import urljoin
 
-class SqliUrlCollectorSpider(scrapy.Spider):
+
+class SQLiURLCollectorSpider(Spider):
     name = "sqli_url_collector"
-    allowed_domains = ["example.com"]
-    start_urls = ["https://example.com/search?q=test"]
+    start_urls = [
+        "https://www.disneyplus.com/identity/login"
+    ]
+
+    # Palabras clave para filtrar URLs
+    keywords = [
+        "/api/login", "access_token",
+        "/login.php", "username", "password",
+        "/signin", "intitle:Login",
+        "/admin/login", "panel",
+        "/wp-login.php", "site:.org",
+        "/user/login", "intitle:Drupal",
+        "index.php?page=login",
+        "/auth", "intext:token",
+        "/account/signin", "site:.co",
+        "login", "AND intext:remember me"
+    ]
 
     def parse(self, response):
-        urls = response.css("a::attr(href)").getall()
-        for url in urls:
-            if "?" in url and "=" in url:
-                yield {"sqli_candidate_url": response.urljoin(url)}
+        # Extraer todos los enlaces de la página
+        links = response.css("a::attr(href)").getall()
+        for link in links:
+            absolute_url = urljoin(response.url, link)
+            # Filtrar URLs que contengan alguna de las palabras clave
+            if any(keyword in absolute_url for keyword in self.keywords):
+                yield {"url": absolute_url}
+
+        # Seguir a la siguiente página si existe
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
